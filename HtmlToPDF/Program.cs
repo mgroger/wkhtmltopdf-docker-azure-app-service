e8b6edb7162d3d38c0d0c4ae650fa8ea6e3ee4a8
Moishe <moishe@tentaroo.com>
2019-06-12 12:14:43 -1000

    Disable request size limit

--- HtmlToPDF/Program.cs
+++ HtmlToPDF/Program.cs
@@ -20,6 +20,10 @@ namespace HtmlToPDF
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseKestrel(options =>
+                {
+                    options.Limits.MaxRequestBodySize = null;
+                })
                 .Build();
     }
 }
